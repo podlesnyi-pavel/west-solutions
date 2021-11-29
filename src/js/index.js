29d43c830753b09f14280a002e41d7f7ddfc77ed
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-11-29 19:33:47 +0200

    form search created

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,24 @@
-const subscribe = document.querySelector('.subscribe__button');
+const subscribe = document.querySelector('.subscribe__form');
+const input = subscribe.querySelector('#input');
 
-subscribe.addEventListener('click', (e) => e.preventDefault());
+subscribe.addEventListener('submit', (e) => {
+  e.preventDefault();
+  input.value = '';
+  input.classList.remove('input--invalid');
+});
+
+input.addEventListener('input', () => {
+  if (input.value !== '') {
+    input.classList.add('input--invalid');
+  }
+
+  if (input.value === '') {
+    input.classList.remove('input--invalid');
+  }
+});
+
+const carsBtn = subscribe.querySelector('.offers__btn');
+
+carsBtn.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
